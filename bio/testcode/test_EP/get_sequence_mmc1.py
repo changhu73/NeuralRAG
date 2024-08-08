f4a70a69adc08e155209f4405706c80030d817d6
ChangHu <66352520+changhu73@users.noreply.github.com>
2024-08-08 12:33:32 +0800

    change the directory of data/datasets

--- bio/testcode/test_EP/get_sequence_mmc1.py
+++ bio/testcode/test_EP/get_sequence_mmc1.py
@@ -93,7 +93,7 @@ def find_max_min(promoter_start, promoter_end, enhancer_start, enhancer_end):
     return maximum, minimum
 
 if __name__ == "__main__":
-    file_name = "c:/Users/13772/OneDrive/Desktop/BU/bio/Gasperini/mmc1.xlsx"
+    file_name = "C:/Users/13772/OneDrive/文档/GitHub/NeuralRAG/bio/Gasperini/mmc1.xlsx"
     sheet_index = 2
 
     ENSG_index = 1
@@ -143,7 +143,7 @@ if __name__ == "__main__":
 
 
     # get sequence between promoter/enhancer
-    output_folder = 'output_files_mmc1'
+    output_folder = 'dataset/output_files_mmc1'
     os.makedirs(output_folder, exist_ok=True)
 
     for i in range(len(ENSG_data)):
